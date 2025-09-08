46f6cca8451b916564b01e6d93f9028c67b379c3
nbazile <narmernbazile@gmail.com>
2025-09-07 22:05:34 -0400

    Complete exercise 11

--- assignment2/2.11.cpp
+++ assignment2/2.11.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
 int max_profit(vector<int>& prices)
 {
-  return 0;
+  int n = prices.size();
+  int profit = 0;
+  int i = 0;
+  for (int j = i + 1; j < n; j++)
+  {
+    if (prices[j] < prices[i])
+    {
+      i = j;
+    }
+    if (prices[j] - prices[i] > profit)
+    {
+      profit = prices[j] - prices[i];
+    }
+  }
+  return profit;
 }
 
 int main()
 {
-  vector<int> v = {7, 1, 5, 3, 6, 4};
+  vector<int> v = {2, 4, 1};
   int _ = max_profit(v);
   cout << _ << endl;
 }
